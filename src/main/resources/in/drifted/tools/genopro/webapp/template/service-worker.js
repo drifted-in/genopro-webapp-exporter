cea53a1144a73bde41f686735a4607d7a19fc5b6
jan-tosovsky-cz <jan.tosovsky.cz@gmail.com>
2018-11-10 22:02:49 +0100

    Ignoring query strings when matching cached URLs

--- src/main/resources/in/drifted/tools/genopro/webapp/template/service-worker.js
+++ src/main/resources/in/drifted/tools/genopro/webapp/template/service-worker.js
@@ -41,7 +41,9 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener("fetch", function(event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request, {
+            ignoreSearch: true
+        }).then(function(response) {
             if (response) {
                 return response;
             } else {
